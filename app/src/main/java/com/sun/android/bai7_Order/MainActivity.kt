9a40ae61e6269f98810863c99d07a703bb288aa0
Dinhduy01 <dinduy2012001@gmail.com>
2022-12-27 15:29:04 +0700

    tab

--- app/src/main/java/com/sun/android/bai7_Order/MainActivity.kt
+++ app/src/main/java/com/sun/android/bai7_Order/MainActivity.kt
@@ -1,62 +1,47 @@
-package com.sun.android.bai5_menu
+package com.sun.android.bai7_Order
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.*
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import com.sun.android.R
-import com.sun.android.databinding.ActivityMain5Binding
+import com.sun.android.databinding.ActivityMain7Binding
 
 class MainActivity : AppCompatActivity() {
-    val binding by lazy { ActivityMain5Binding.inflate(layoutInflater) }
+    val binding by lazy { ActivityMain7Binding.inflate(layoutInflater) }
+    var mOrderMessage: String = ""
+    val EXTRA_MESSAGE: String = "MESSAGE"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        registerForContextMenu(binding.article)
-    }
-
-    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-        val menuInflater: MenuInflater = getMenuInflater()
-        menuInflater.inflate(R.menu.menu_context, menu)
-    }
-
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.context_edit -> {
-                displayToast(getString(R.string.edit_message))
-            }
-            R.id.context_share -> {
-                displayToast(getString(R.string.share_message))
-            }
-            R.id.context_delete -> {
-                displayToast(getString(R.string.delete_message))
-            }
-        }
-        return super.onContextItemSelected(item)
-    }
-
-    fun displayToast(message: String) {
-        Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+        binding.fab.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, OrderActivity::class.java)
+            intent.putExtra(EXTRA_MESSAGE, mOrderMessage)
+            startActivity(intent)
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.main_menu, menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         val nightMode: Int = AppCompatDelegate.getDefaultNightMode()
         if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
             menu.findItem(R.id.night_mode).setTitle(R.string.day_mode)
         } else {
             menu.findItem(R.id.night_mode).setTitle(R.string.night_mode)
         }
-        val item: MenuItem = menu.findItem(R.id.bai_5)
+        val item: MenuItem = menu.findItem(R.id.bai_7)
         item.isVisible = false
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.night_mode) {
+        if (item.itemId == R.id.action_settings) {
+            return true
+        } else if (item.itemId == R.id.night_mode) {
             val nightMode: Int = AppCompatDelegate.getDefaultNightMode()
             if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
@@ -77,16 +62,36 @@ class MainActivity : AppCompatActivity() {
         } else if (item.itemId == R.id.bai_4) {
             val intent = Intent(this, com.sun.android.bai4_Drawable.MainActivity::class.java)
             startActivity(intent)
+        } else if (item.itemId == R.id.bai_5) {
+            val intent = Intent(this, com.sun.android.bai5_menu.MainActivity::class.java)
+            startActivity(intent)
         } else if (item.itemId == R.id.bai_6) {
             val intent = Intent(this, com.sun.android.bai6_dialog_date_time.MainActivity::class.java)
             startActivity(intent)
-        }
-        else if(item.itemId == R.id.bai_7){
-            val intent = Intent(this,com.sun.android.bai7_Order.MainActivity::class.java)
+        } else if (item.itemId == R.id.bai_7) {
+            val intent = Intent(this, com.sun.android.bai7_Order.MainActivity::class.java)
             startActivity(intent)
         }
-        return true
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun displayToast(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun showDonutOrder(view: View) {
+        mOrderMessage = getString(R.string.donut_order_message)
+        displayToast(mOrderMessage)
+    }
+
+    fun showIceCreamOrder(view: View) {
+        mOrderMessage = getString(R.string.ice_cream_order_message)
+        displayToast(mOrderMessage)
+    }
+
+    fun showFroyoOrder(view: View) {
+        mOrderMessage = getString(R.string.froyo_order_message)
+        displayToast(mOrderMessage)
     }
 
 }
-
